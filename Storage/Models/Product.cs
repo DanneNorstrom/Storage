bb3ce25a571f344fbaeb29166af0c9391a60ef37
dannor0608 <93254330+dannor0608@users.noreply.github.com>
2024-10-27 21:29:29 +0100

    Data annotations added

--- Storage/Models/Product.cs
+++ Storage/Models/Product.cs
@@ -1,15 +1,23 @@
 ﻿
 
+using System.ComponentModel.DataAnnotations;
+
 namespace Storage.Models
 {
     public class Product
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
+
+        [Range(1, int.MaxValue, ErrorMessage = "Price must be greater than 0")]
         public int Price { get; set; }
+
+        [DataType(DataType.Date)]
         public  DateTime Orderdate { get; set; }
         public string Category { get; set; } = string.Empty;
         public string Shelf { get; set; } = string.Empty;
+
+        [Range(0, int.MaxValue, ErrorMessage = "Count must be 0 or greater")]
         public int Count { get; set; }
         public string Description { get; set; } = string.Empty;
     }
